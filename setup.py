18545e7c997e11bb4c783370834e3a3e3391f72a
Matt Miller <mattmiller25@gmail.com>
2015-04-01 12:59:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.4'
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
