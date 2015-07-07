e1a086292a0f9707da260c02f1793f3afa93046a
Matthew Miller <matthew.miller@refinery29.com>
2015-07-07 12:26:03 -0400

    debugging Travis

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 pythonversion=$(python -c 'import sys; print("%d.%d.%d" % (sys.version_info[0], sys.version_info[1], sys.version_info[2]))')
 
 if [[ $pythonversion =~ ^3.*$ ]]; then
@@ -12,5 +11,3 @@ if [[ $pythonversion =~ ^2.*$ ]]; then
 	echo "Install dependencies using Python 2"
 	pip install -r chassis/development.txt
 fi
-
-# Making a change here.
